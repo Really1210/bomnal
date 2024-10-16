6dd557b2db5aeabe49039ca00f867830da280f24
Really1210 <whchoi86@naver.com>
2024-10-16 07:54:32 +0000

    사진 추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("🎈 봄날의꽃 BOMNANL Flower")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+st.image(image_url, caption='https://pcmap.place.naver.com/place/1334572327/photo?entry=pll&from=map&fromNxList=true&fromPanelNum=2&timestamp=202410161652#', use_column_width=True)
