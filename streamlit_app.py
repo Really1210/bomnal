b58cc9b6c4efeca261b1586aa181748a0f565961
Really1210 <whchoi86@naver.com>
2024-10-16 07:56:58 +0000

    사진 주소 변경

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,8 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
+# 이미지 URL을 변수로 저장
+image_url = 'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20240613_190%2F1718262533822XFxk4_JPEG%2FKakaoTalk_20240613_160008244.jpg'
 
-st.image(image_url, caption='https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20240613_190%2F1718262533822XFxk4_JPEG%2FKakaoTalk_20240613_160008244.jpg', use_column_width=True)
+# st.image()에 이미지 URL 전달 및 캡션 추가
+st.image(image_url, caption='봄날의꽃', use_column_width=True)
